14ac32a09642500a948c2f59315d8d4e60fc46d3
JackAttack612 <jetflyer1130@gmail.com>
2022-01-14 18:44:13 -0500

    Fixing Logs

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from nextcord.ext import commands
 import BotConfig
 import json
 import os
+import os.path
 
 #Fetching Logging Channel
 def get_log(client, message):
@@ -22,6 +23,8 @@ BOTPREFIX=BotConfig.BOT_PREFIX
 client = commands.Bot(command_prefix = BOTPREFIX, intents=intents)
 client.remove_command("help")
 
+#Finding File Directory
+directory = os.path.dirname(__file__)
 
 #Getting Bot Prefix From Config File
 TOKEN=BotConfig.DISCORD_BOT_TOKEN
@@ -36,14 +39,14 @@ async def on_ready():
 #Creating logging channel file when the bot joins a server
 @client.event
 async def on_guild_join(guild):
-    f = open(f"Logs/{guild.id}", 'w+')
+    f = open(f"{directory}/Logs/{guild.id}", 'w+')
     f.write("None")
     f.close
 
 #Removing file when bot leaves the server
 @client.event
 async def on_guild_remove(guild):
-    os.remove(f"Logs/{guild.id}")
+    os.remove(f"{directory}/Logs/{guild.id}")
     print(f"{guild.id} was removed from logs")
 
 #Setting the logging channel command
@@ -66,14 +69,14 @@ async def logchannel(ctx, *, logset=None):
         logset = check2
         print(logset)
 
-    file_exists = os.path.exists(f'Logs/{ctx.guild.id}.txt')
+    file_exists = os.path.exists(f'{directory}/Logs/{ctx.guild.id}.txt')
     if (file_exists == True):
-        os.remove(f"Logs/{ctx.guild.id}")
-        f = open(f"Logs/{ctx.guild.id}", "w+")
+        os.remove(f"{directory}/Logs/{ctx.guild.id}")
+        f = open(f"{directory}/Logs/{ctx.guild.id}", "w+")
         f.write(f"{logset}")
         f.close
     if (file_exists == False):
-        f = open(f"Logs/{ctx.guild.id}", "w+")
+        f = open(f"{directory}/Logs/{ctx.guild.id}", "w+")
         f.write(f"{logset}")
         f.close
     await ctx.send(f"Logging Channel set to <#{logset}>.")
@@ -93,14 +96,14 @@ async def ban(ctx, member: nextcord.Member, *, reason=None):
     await ctx.send(f'User {member.mention} has been banned. Reason: {reason}')
 
     #Sending Logging Message
-    f = open(f"Logs/{ctx.guild.id}", 'r')
+    f = open(f"{directory}/Logs/{ctx.guild.id}", 'r')
     if f.mode == 'r':
         logs = await ctx.guild.audit_logs(limit=1, action=nextcord.AuditLogAction.ban).flatten()
         contents = f.read()
         contentsint = int(contents)
         channel = client.get_channel(contentsint)
         logs = logs[0]
-        await channel.send(f"{member.mention} was banned by {ctx.author.mention} for {reason}")
+        await channel.send(f"{member.mention} was banned by {ctx.author.mention}. Reason: {reason}")
         f.close
     elif f.mode != 'r':
         f.close
@@ -130,14 +133,14 @@ async def kick(ctx, member: nextcord.Member, *, reason=None):
     await ctx.send(f'User {member.mention} has been kicked. Reason: {reason}')
 
     #Sending Logging Message
-    f = open(f"Logs/{ctx.guild.id}", 'r')
+    f = open(f"{directory}/Logs/{ctx.guild.id}", 'r')
     if f.mode == 'r':
         logs = await ctx.guild.audit_logs(limit=1, action=nextcord.AuditLogAction.ban).flatten()
         contents = f.read()
         contentsint = int(contents)
         channel = client.get_channel(contentsint)
         logs = logs[0]
-        await channel.send(f"{member.mention} was kicked by {ctx.author.mention} for {reason}")
+        await channel.send(f"{member.mention} was kicked by {ctx.author.mention}. Reason: {reason}")
         f.close
     elif f.mode != 'r':
         f.close
